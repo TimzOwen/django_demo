bcaa51a8236bda247eff03a7d264cf2dcf2d8b9e
TimzOwen <ktimothy@kabarak.ac.ke>
2022-03-28 13:37:00 +0300

    using GET methods

--- calc/views.py
+++ calc/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def home(request):
+    return render(request, 'home.html', {'name': 'Timz Owen'})
 
-    return render(request, 'home.html',{'name':'Timz Owen'})
+
+# function to do the addition operations
+def add(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    result = val2 + val1
+
+    return render(request, 'results.html', {'results': result})
